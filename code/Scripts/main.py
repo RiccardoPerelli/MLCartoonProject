e70ebe76312e68c28d2cf50282be3111f3fdeea5
Riccardo Bumme <s277505@studenti.polit.it>
2020-12-12 14:24:07 +0100

    project set-up almost finished (GPU support needed)

---
+++ code/Scripts/main.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
+
+print(tf.version)
